12aabc5d89654fa5443269a85e563e0d1f295d17
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-10-06 17:46:16 +0100

    h2-sub title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,10 +13,16 @@ const Hero = () => {
             </button>
             </nav>
             <h1 className='header_text'>
-              Summarize Articles with <br />
+              Summarize Articles with <br className='max-md:hidden' />
               <span className='orange_gradient'> OpenAi GPT-4</span>
 
             </h1>
+            <h2 className='praise'>
+            JSX elements are treated as JavaScript expressions. They can go anywhere that JavaScript expressions can go. This means that a JSX element can be saved in a variable, passed to a function, stored in an object or array… you name it.
+
+Here’s an example of a JSX element being saved in a variable:
+
+            </h2>
 
     </header>
   )
