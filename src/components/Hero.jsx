67a1c8b8586cce04bd3c97df2bbc77826ab1e194
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-10-06 17:34:37 +0100

    color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
             </nav>
             <h1 className='header_text'>
               Summarize Articles with <br />
-              <span className='blue_gradient'> OpenAi GPT-4</span>
+              <span className='orange_gradient'> OpenAi GPT-4</span>
 
             </h1>
 
