56c29214981f4a2920551e6f1b8e84e4f3454170
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-10-06 15:24:03 +0100

    nav,img,button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,12 @@ const Hero = () => {
     <header className='w-full flex justify-center items-center flex-col '>
         <nav className='flex justify-between items-center flex-col '>
             <img src='{logo}' alt='sumz_logo' className='w-28 object-contain'/>
+            <button type='button'onClick={() => window.open('https://github.com/Ejiscoxpres')}
+            className='black_btn'
+            >
+              Github
+
+            </button>
             </nav>
 
     </header>
